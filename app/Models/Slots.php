1b4445b348ca6015f20420c69ea1873afb9058ef
Antoine SCHILT <70049513+mediatorviolet@users.noreply.github.com>
2021-03-17 19:36:53 +0400

    association

--- app/Models/Slots.php
+++ app/Models/Slots.php
@@ -16,4 +16,20 @@ class Slots extends Model
         'heure_debut',
         'heure_fin'
     ];
+
+    /**
+     * Get the ordinateur associated with the slot
+     */
+    public function ordinateur()
+    {
+        return $this->hasOne(Ordinateurs::class, 'foreign_key');
+    }
+
+    /**
+     * Get the utilisateur associated with the slot
+     */
+    public function utilisateur()
+    {
+        return $this->hasOne(Utilisateurs::class, 'foreign_key');
+    }
 }

5827e2de78ac97c806c7c537c68c87a83f66d95d
Antoine SCHILT <70049513+mediatorviolet@users.noreply.github.com>
2021-03-17 12:35:54 +0400

    responsive + pagination

--- resources/views/ordinateurs/index.blade.php
+++ resources/views/ordinateurs/index.blade.php
@@ -8,10 +8,20 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200 grid grid-cols-3">
+                <div class="p-6 bg-white border-b border-gray-200 grid grid-cols-3 grid-rows-3">
                     <div class="col-span-2">
                         <h3 class="font-semibold text-lg text-gray-800 py-2">Liste des ordinateurs</h3>
-                        <table class="table-fixed w-3/4">
+                    </div>
+                    <div>
+                        <p class="text-right">
+                            <a href="ordinateurs/create"
+                                class="border-b-2 border-transparent text-2xl text-indigo-500 hover:font-semibold hover:text-indigo-700 transition duration-150 ease-in-out">
+                                <i class="fas fa-plus"></i>
+                            </a>
+                        </p>
+                    </div>
+                    <div class="col-span-3 row-span-2">
+                        <table class="table-fixed w-full mb-6">
                             <tbody>
                                 @forelse ($ordinateurs as $ordinateur)
                                     <tr class="py-2 flex justify-between border-b-2 mt-2">
@@ -25,7 +35,8 @@
                                                     actif
                                                 </span>
                                             @else
-                                                <span class="px-2 inline-flex leading-5 rounded-full bg-red-100 text-red-800">
+                                                <span
+                                                    class="px-2 inline-flex leading-5 rounded-full bg-red-100 text-red-800">
                                                     inactif
                                                 </span>
                                             @endif
@@ -40,7 +51,8 @@
                                             <form action="ordinateurs/{{ $ordinateur->id }}" method="post">
                                                 @csrf
                                                 @method('DELETE')
-                                                <button type="submit" class="text-gray-800 hover:text-red-500 transition duration-150 ease-in-out">
+                                                <button type="submit"
+                                                    class="text-gray-800 hover:text-red-500 transition duration-150 ease-in-out">
                                                     <i class="fas fa-trash"></i>
                                                 </button>
                                             </form>
@@ -51,14 +63,7 @@
                                 @endforelse
                             </tbody>
                         </table>
-                    </div>
-                    <div>
-                        <p class="text-right">
-                            <a href="ordinateurs/create"
-                                class="border-b-2 border-transparent text-indigo-500 hover:font-semibold hover:border-indigo-700 hover:text-indigo-700 transition duration-150 ease-in-out">
-                                Créer un nouvel ordinateur &plus;
-                            </a>
-                        </p>
+                        {{ $ordinateurs->links() }}
                     </div>
                 </div>
             </div>

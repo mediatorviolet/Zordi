9a3ff8cf74ee56cdb982aa4b2144eb0ded8b49bc
Antoine SCHILT <70049513+mediatorviolet@users.noreply.github.com>
2021-03-19 13:54:51 +0400

    DB seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +15,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('horaires')->insert([
+            'plage_horaire' => '8h - 10h'
+        ]);
+
+        DB::table('horaires')->insert([
+            'plage_horaire' => '10h - 12h'
+        ]);
+
+        DB::table('horaires')->insert([
+            'plage_horaire' => '14h - 16h'
+        ]);
+
+        DB::table('horaires')->insert([
+            'plage_horaire' => '16h - 18h'
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@mail.com',
+            'password' => Hash::make('test1234')
+        ]);
     }
 }
